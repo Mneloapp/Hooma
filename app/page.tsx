cfceb9d06d6f7e3c24355ab32bb163a0e79da7e8
Mneloapp <shp6tpt7mz@privaterelay.appleid.com>
2026-07-19 14:24:29 +0400

    Paginate storefront product grids

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { HomeStorefrontClient } from "@/components/home/HomeStorefrontClient";
+import { catalogCategories } from "@/data/catalog";
 import { getDailyDealDiscountPercent } from "@/lib/daily-deals";
 import { getStorefrontCatalog } from "@/lib/storefront-catalog";
 
@@ -9,5 +10,18 @@ export default async function Home() {
     getStorefrontCatalog(),
     getDailyDealDiscountPercent(),
   ]);
-  return <HomeStorefrontClient catalogProducts={catalogProducts} dailyDealDiscountPercent={dailyDealDiscountPercent} />;
+  const homeProducts = new Map(
+    [...catalogProducts]
+      .sort((left, right) => right.popularityScore - left.popularityScore || right.salesCount - left.salesCount || right.ratingAverage - left.ratingAverage)
+      .slice(0, 12)
+      .map((product) => [product.id, product]),
+  );
+  for (const category of catalogCategories) {
+    catalogProducts
+      .filter((product) => product.categorySlug === category.slug)
+      .slice(0, 12)
+      .forEach((product) => homeProducts.set(product.id, product));
+  }
+
+  return <HomeStorefrontClient catalogProducts={[...homeProducts.values()]} dailyDealDiscountPercent={dailyDealDiscountPercent} />;
 }
